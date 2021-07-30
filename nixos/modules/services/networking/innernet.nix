006b795383c71c6ca7ddda0e9f262da8f363b5c3
Philipp Herzog <philipp.herzog@protonmail.com>
2021-07-30 19:26:58 +0200

    add port option + interface pattern matching from filename to innernet service

--- nixos/modules/services/networking/innernet.nix
+++ nixos/modules/services/networking/innernet.nix
@@ -10,23 +10,22 @@ in {
   options.services.innernet = with types; {
     enable = mkEnableOption "innernet client daemon";
 
-    #port = mkOption {
-      #type = port;
-      #default = 41641;
-      #description = "The port to listen on for tunnel traffic (0=autoselect).";
-    #};
+    port = mkOption {
+      type = port;
+      default = 51820;
+      description = "The port to listen on for tunnel traffic";
+    };
 
-    interfaceName = mkOption {
-      type = str;
-      default = "tailscale0";
-      description = ''The interface name for tunnel traffic. Use "userspace-networking" (beta) to not use TUN.'';
+    configFile = mkOption {
+      type = path;
+      description = "Path to the config file for the innernet server interface";
     };
 
     package = mkOption {
       type = package;
-      default = pkgs.tailscale;
+      default = pkgs.innernet;
       defaultText = "pkgs.innernet";
-      description = "The package to use for tailscale";
+      description = "The package to use for innernet";
     };
 
     openFirewall = mkOption {
@@ -35,20 +34,29 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
+  config = let
+    interfaceName = builtins.head (builtins.match "[a-zA-Z_/-]+/([a-zA-Z_-]+).conf");
+  in mkIf cfg.enable {
     networking.wireguard.enable = true;
-    #networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [ cfg.port ];
+    networking.firewall.allowedTCPPorts = mkIf cfg.openFirewall [ cfg.port ];
 
     environment.systemPackages = [ cfg.package ]; # for the CLI
+    environment.etc = {
+      "innernet-server/${interfaceName}.conf" = {
+        mode = "0644"; text = fileContents "${cfg.configFile}";
+      };
+    };
+
     systemd.packages = [ cfg.package ];
     systemd.services.innernetd = {
       after = [ "network-online.target" "nss-lookup.target" ];
       wantedBy = [ "multi-user.target" ];
 
       path = [ pkgs.iproute ];
+      environment = { RUST_LOG = "info"; };
       serviceConfig =  {
         Restart = "always";
-        ExecStart = "${cfg.package}/bin/innernet-server serve ${cfg.interfaceName}";
+        ExecStart = "${cfg.package}/bin/innernet-server serve ${interfaceName}";
       };
     };
   };
